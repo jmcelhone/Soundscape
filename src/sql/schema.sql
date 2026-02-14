16484e8c40a33c5c4d290b2757f0c171d60dee3c
leoSallos <sallosl@oregonstate.edu>
2026-02-13 17:30:06 -0800

    added a draft of the database schema

---
+++ src/sql/schema.sql
@@ -0,0 +1,46 @@
+/* Users Table */
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(36) NOT NULL,
+    `password` VARCHAR(40) NOT NULL,
+    `creationDate` TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (`id`)
+);
+
+/* Posts Table */
+DROP TABLE IF EXISTS `Posts`;
+CREATE TABLE `Posts` (
+    `userID` INT NOT NULL,
+    `time` TIMESTAMP NOT NULL,
+    `songID` INT,
+    `location` POINT,
+    `comment` JSON,
+
+    PRIMARY KEY (`userID`, `time`),
+    FOREIGN KEY (`userID`) REFERENCES `Users` ON DELETE CASCADE
+);
+
+/* Friends Table */
+DROP TABLE IF EXISTS `Friends`;
+CREATE TABLE `Friends` (
+    `userID1` INT NOT NULL,
+    `userID2` INT NOT NULL,
+
+    PRIMARY KEY (`userID1`, `userID2`),
+    FOREIGN KEY (`userID1`) REFERENCES `Users` ON DELETE CASCADE,
+    FOREIGN KEY (`userID2`) REFERENCES `Users` ON DELETE CASCADE
+);
+
+/* Activity Table */
+DROP TABLE IF EXISTS `UserActivity`;
+CREATE TABLE `UserActivity` (
+    `userID` INT NOT NULL,
+    `songID` INT,
+    `location` POINT,
+    `PrivacyStatus` VARCHAR(16) NOT NULL,
+
+    PRIMARY KEY (`userID`),
+    FOREIGN KEY (`userID`) REFERENCES `Users` ON DELETE CASCADE
+);
