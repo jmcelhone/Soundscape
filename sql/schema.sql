8fccfc1da88ca418dd1b26522ceff42a73040491
leoSallos <sallosl@oregonstate.edu>
2026-02-15 14:19:14 -0800

    updated schema to new specification with auth schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,46 +1,45 @@
 /* Users Table */
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
-    id SERIAL NOT NULL,
-    username VARCHAR(36) NOT NULL,
-    password VARCHAR(40) NOT NULL,
-    creationDate TIMESTAMP NOT NULL,
+DROP TABLE IF EXISTS userSettings;
+CREATE TABLE userSettings (
+    id UUID PRIMARY KEY,
+    username VARCHAR (50),
+    locationStatus SMALLINT NOT NULL,
+    nowPlayingStatus SMALLINT NOT NULL,
+    defaultPostStatus SMALLINT NOT NULL,
 
-    PRIMARY KEY (id)
+    FOREIGN KEY (id) REFERENCES auth.users ON DELETE CASCADE
 );
 
 /* Posts Table */
-DROP TABLE IF EXISTS Posts;
-CREATE TABLE Posts (
-    userID INT NOT NULL,
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+    postID BIGSERIAL NOT NULL PRIMARY KEY,
+    userID UUID NOT NULL,
     time TIMESTAMP NOT NULL,
     songID INT,
     location POINT,
     comment JSON,
 
-    PRIMARY KEY (userID, time),
-    FOREIGN KEY (userID) REFERENCES Users ON DELETE CASCADE
+    FOREIGN KEY (userID) REFERENCES auth.users ON DELETE CASCADE
 );
 
 /* Friends Table */
 DROP TABLE IF EXISTS Friends;
 CREATE TABLE Friends (
-    userID1 INT NOT NULL,
-    userID2 INT NOT NULL,
+    userID1 UUID NOT NULL,
+    userID2 UUID NOT NULL,
 
     PRIMARY KEY (userID1, userID2),
-    FOREIGN KEY (userID1) REFERENCES Users ON DELETE CASCADE,
-    FOREIGN KEY (userID2) REFERENCES Users ON DELETE CASCADE
+    FOREIGN KEY (userID1) REFERENCES auth.users ON DELETE CASCADE,
+    FOREIGN KEY (userID2) REFERENCES auth.users ON DELETE CASCADE
 );
 
 /* Activity Table */
 DROP TABLE IF EXISTS UserActivity;
 CREATE TABLE UserActivity (
-    userID INT NOT NULL,
+    userID UUID NOT NULL PRIMARY KEY,
     songID INT,
     location POINT,
-    PrivacyStatus VARCHAR(16) NOT NULL,
 
-    PRIMARY KEY (userID),
-    FOREIGN KEY (userID) REFERENCES Users ON DELETE CASCADE
+    FOREIGN KEY (userID) REFERENCES auth.users ON DELETE CASCADE
 );
