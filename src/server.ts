cc69acbaff6f6218228a516d2161a051f4ce10fb
leoSallos <sallosl@oregonstate.edu>
2026-02-14 17:30:54 -0800

    changed server to use HTTPS instead of HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+import * as fs from 'fs';
+import * as https from 'https';
 import app from './app.ts';
 
 const port: Number = Number(process.env.PORT!);
 
+// get HTTPS options
+const options: https.ServerOptions = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+    passphrase: process.env.HTTPS_KEY_PASSPHRASE!
+};
+
 // start server
-app.listen(port, function(err){
-    if (err){
-        throw err;
-    } else {
-        console.log("Server listening on port " + port);
-    }
+https.createServer(options, app).listen(port, () => {
+    console.log("Server listening on port " + port);
 });
