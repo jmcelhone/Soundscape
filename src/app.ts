96ad4f180cdcbb7a53a40e0c9eba51e91c96e80c
leoSallos <sallosl@oregonstate.edu>
2026-02-15 13:02:03 -0800

    implemented OAuth calling and user creation, login cookies not implemented and marked TODO

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,79 @@
 import express, { type Request, type Response } from 'express';
 import { createClient, type QueryData, type QueryError } from '@supabase/supabase-js';
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr';
 import { type Database } from './supabase.ts';
 
 // create express app
 const app = express()
+
+// middleware
 app.use(express.json())
 
+// create supabase database
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
-
-// create supabase database
 const supabase = createClient<Database>(
     supabaseURL,
     supabaseKey
 );
 
 // routing
+app.get("/auth/spotify/login", async (req: Request, res: Response) => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'spotify',
+        options: {
+            redirectTo: 'https://localhost:8000/auth/callback'
+        }
+    });
+
+    if (error) {
+        console.error(error);
+        res.status(500).send("Error: server error on gathering auth redirect.");
+    } else {
+        res.redirect(303, data.url);
+    }
+});
+
+app.get("/auth/callback", async (req: Request, res: Response) => {
+    const code = req.query.code;
+    const next = req.query.next ?? "/";
+
+    // TODO *****************************
+    // Integrate persist login with OAuth
+    // **********************************
+    
+    // Debug ************************************
+    console.log("Code:", code, "\n Next:", next);
+    // ******************************************
+    
+    if (code) {
+        const supabase = createServerClient(
+            supabaseURL,
+            supabaseKey,
+            {
+                cookies: {
+                    getAll() {
+                        return parseCookieHeader(req.headers.cookie ?? '');
+                    },
+                    setAll(cookiesToSet) {
+                        cookiesToSet.forEach(({ name, value, options }) =>
+                            res.appendHeader('Set-Cookie', serializeCookieHeader(name, value, options))
+                        );
+                    },
+                },
+            }
+        );
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+    res.redirect(303, `/${next.slice(1)}`);
+});
+
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send("Hello World");
 });
 
-/* Example for making a query
-const usersQuery = supabase
-    .from("users")
-    .select("*");
-type AllUsers = QueryData<typeof usersQuery>;
-const { data, error } = await usersQuery;
-if (error) throw error;
-const users: AllUsers = data;
-
-console.log(users);
-*/
+app.use((req: Request, res: Response) => {
+    res.status(404).send("Error: 404");
+});
 
 export default app;
