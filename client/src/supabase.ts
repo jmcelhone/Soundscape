f1acde7cb23aa59f66868441f50300226c0c29c4
leoSallos <sallosl@oregonstate.edu>
2026-02-27 20:52:02 -0800

    patched typo in vite .env reading

--- client/src/supabase.ts
+++ client/src/supabase.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseURL = import.meta.env.VITE_PUBLIC_SUPABASE_URL!;
 const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
-const cookieHostURL = import.meta.env.COOKIE_HOST_URL!;
+const cookieHostURL = import.meta.env.VITE_COOKIE_HOST_URL!;
 const isProd = import.meta.env.NODE_ENV;
 
 export function createClient() {
