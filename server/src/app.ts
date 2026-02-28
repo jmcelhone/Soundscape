e85561438d78085e8f76c986b686825aec9a0883
leoSallos <sallosl@oregonstate.edu>
2026-02-27 19:09:34 -0800

    enable server cors for all sites

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,11 +7,7 @@ import cors from "cors";
 const app = express()
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+app.use(cors());
 app.use(express.json())
 
 app.post("/posts", async (req: Request, res: Response) => {
