c7688ff277c6e8706a5259ea0afde4c834fa2fc0
leoSallos <sallosl@oregonstate.edu>
2026-03-07 14:22:35 -0800

    updated scheme to what we will be using at this point in the project

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -1,11 +1,8 @@
 /* Users Table */
-DROP TABLE IF EXISTS userSettings;
-CREATE TABLE userSettings (
+DROP TABLE IF EXISTS userNames;
+CREATE TABLE userNames (
     id UUID PRIMARY KEY,
-    username VARCHAR (50),
-    locationStatus SMALLINT NOT NULL,
-    nowPlayingStatus SMALLINT NOT NULL,
-    defaultPostStatus SMALLINT NOT NULL,
+    username VARCHAR (50) NOT NULL,
 
     FOREIGN KEY (id) REFERENCES auth.users ON DELETE CASCADE
 );
@@ -24,27 +21,6 @@ CREATE TABLE posts (
     longitude DOUBLE PRECISION NOT NULL,
 
     comment TEXT,
-
-    FOREIGN KEY (userID) REFERENCES auth.users ON DELETE CASCADE
-);
-
-/* Friends Table */
-DROP TABLE IF EXISTS Friends;
-CREATE TABLE Friends (
-    userID1 UUID NOT NULL,
-    userID2 UUID NOT NULL,
-
-    PRIMARY KEY (userID1, userID2),
-    FOREIGN KEY (userID1) REFERENCES auth.users ON DELETE CASCADE,
-    FOREIGN KEY (userID2) REFERENCES auth.users ON DELETE CASCADE
-);
-
-/* Activity Table */
-DROP TABLE IF EXISTS UserActivity;
-CREATE TABLE UserActivity (
-    userID UUID NOT NULL PRIMARY KEY,
-    songID INT,
-    location POINT,
 
     FOREIGN KEY (userID) REFERENCES auth.users ON DELETE CASCADE
 );
